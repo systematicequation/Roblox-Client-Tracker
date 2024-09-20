346bf467e0e671d9b290a8b286eec4d414b8437e
systematicequation <56933490+systematicequation@users.noreply.github.com>
2024-09-20 03:43:59 -0700

    Delete BuiltInStandalonePlugins directory

--- BuiltInStandalonePlugins/Debugger/Src/Models/Watch/TableTab.luac.s
+++
@@ -1,8 +0,0 @@
-MAIN:
-  PREPVARARGS 0
-  DUPTABLE R0 K2 [{"Variables", "Watches"}]
-  LOADK R1 K0 ["Variables"]
-  SETTABLEKS R1 R0 K0 ["Variables"]
-  LOADK R1 K1 ["Watches"]
-  SETTABLEKS R1 R0 K1 ["Watches"]
-  RETURN R0 1
